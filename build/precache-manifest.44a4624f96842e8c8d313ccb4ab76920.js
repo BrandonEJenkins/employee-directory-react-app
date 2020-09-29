8cb3ff623fceb9706ead0278d5ae68aa976b60e3
BrandonEJenkins <brandonejenkins@gmail.com>
2020-09-29 12:35:40 -0400

    deploying app

---
+++ build/precache-manifest.44a4624f96842e8c8d313ccb4ab76920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2086bcb9a3b2e6a5062600a0be9dd044",
+    "url": "/employee-directory-react-app/index.html"
+  },
+  {
+    "revision": "77e715bbd340060fb869",
+    "url": "/employee-directory-react-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ba920056df215de33451",
+    "url": "/employee-directory-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77e715bbd340060fb869",
+    "url": "/employee-directory-react-app/static/js/2.0f9391a8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-directory-react-app/static/js/2.0f9391a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba920056df215de33451",
+    "url": "/employee-directory-react-app/static/js/main.90db0929.chunk.js"
+  },
+  {
+    "revision": "eae69a2e0f3c55e9abd4",
+    "url": "/employee-directory-react-app/static/js/runtime-main.eb835477.js"
+  }
+]);
